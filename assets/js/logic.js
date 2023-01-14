f9323ffd1c29375a9abba62f37cf235b4f6faa00
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-01-14 14:02:46 +0000

    renaming js file, adding questions.js, texting connection between HTML and js works, testing button click by alert

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+
+// setting variable for start quiz button
+var startQuizBtn = document.getElementById("start");
+// setting variable for start screen div
+var startScreen = document.getElementById("start-screen");
+
+// function to start the quiz
+function startQuiz() {
+alert("You've started the quiz");
+}
+
+startQuizBtn.addEventListener("click", startQuiz);
