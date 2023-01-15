635dd4cda606144696900da124182826baeb54d0
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-01-15 11:07:06 +0000

    adding function to check the answer is correct, displaying message of correct or incorrect, adding line to remove previous q+a from screen for each new question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,36 +10,74 @@ var questionTitle = document.getElementById('question-title');
 // setting variable for choices div
 var questionChoices = document.getElementById('choices');
 
+// setting current question to 0
 var currentQuestionIndex = 0;
 
-
-// function to show the next question
-// function startNextQuestion() {
-//     showQuestion();
-
-// }
-
 function showQuestion() {
+    // Adding question text to questionTitle based on current question index
     questionTitle.innerText = quizQuestions[currentQuestionIndex].question;
+    // Removing previous question and answer
+    questionChoices.innerText = "";
 
+    // Setting variable for answer length
     var answersLength = quizQuestions[currentQuestionIndex].answers.length;
-    var ol = document.createElement("ol");
-    questionChoices.appendChild(ol);
 
+    // creating an ordered list element
+    var ol = document.createElement("ol");
+
+    // Adding ordered list to the choices div
+    questionChoices.appendChild(ol);
+    
     for (i = 0; i < answersLength; i++) {
             
+         // For every answer in question, add a list element
             var li = document.createElement("li");
+            // Set the text to the answer using index
             li.textContent = quizQuestions[currentQuestionIndex].answers[i];
+            // add li to ol element
             ol.appendChild(li);
+            // listen for click of li and run checkAnswer function
             li.addEventListener("click", checkAnswer);
 }
 }
 
+// Function to check users answer and move on to next question
+function checkAnswer(event) {
+
+    // Setting variable for selected answer to whatever the user clicked
+    var selectedAnswer = event.target.innerText;
+
+    // assigning correctAnswer to the correct answer for that question
+    var correctAnswer = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    // Checking to see if user has correct answer
+    if (selectedAnswer === correctAnswer) {
+        alert("Correct");
+    } else {
+        alert("Incorrect. The correct answer is " + correctAnswer);
+    }
+
+    // adding 1 to current question index so it can go through each of the questions
+    currentQuestionIndex++;
+
+    // Checking that there are more questions, if so - go to next question. if not finish quiz
+    if (currentQuestionIndex < quizQuestions.length) {
+        showQuestion();
+    } else {
+        alert("You've completed the quiz!");
+    }
+}
+
+// Add event listener to the answer choices that call the 'checkAnswer' function when clicked
+var answerChoices = document.querySelectorAll("li");
+
+for (i = 0; i < answerChoices.length; i++) {
+    answerChoices[i].addEventListener("click", checkAnswer);
+}
 
 // function which removes 'hide' class and shows the questions screen
 function showQuestionScreen() {
     questionScreen.classList.remove("hide");
-    currentQuestionIndex = 0;
     // startNextQuestion();
     showQuestion();
 }
